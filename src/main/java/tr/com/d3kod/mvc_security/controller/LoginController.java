3f9b93ea68de1b240111488373f59980f4fb93a8
HACI DAYI <hdayi@hotmail.com>
2023-07-19 06:59:38 +0300

    custom login form login/logout tamam

---
+++ src/main/java/tr/com/d3kod/mvc_security/controller/LoginController.java
@@ -0,0 +1,12 @@
+package tr.com.d3kod.mvc_security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "fancy-login";
+    }
+}
--- src/main/java/tr/com/d3kod/mvc_security/security/SecurityConfiguration.java
+++ src/main/java/tr/com/d3kod/mvc_security/security/SecurityConfiguration.java
@@ -2,9 +2,11 @@ package tr.com.d3kod.mvc_security.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -17,5 +19,16 @@ public class SecurityConfiguration {
                 .roles("PERSONEL", "MUDUR", "MUMKUNMUDUR").build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                configurer -> configurer.anyRequest().authenticated()).formLogin(
+                        form -> form.loginPage("/login")
+                                .loginProcessingUrl("/authenticate")
+                                .permitAll())
+                .logout(logout -> logout.permitAll());
+        return http.build();
     }
 }
