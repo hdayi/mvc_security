c888c7d0d726b9e0b147bc2ac2475fa77291f9d5
HACI DAYI <hdayi@hotmail.com>
2023-07-20 03:05:34 +0300

    JDBC Ile security hazir db username ve passordu girmeden guncelleme

--- src/main/java/tr/com/d3kod/mvc_security/controller/AppController.java
+++ src/main/java/tr/com/d3kod/mvc_security/controller/AppController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
+
     @GetMapping("/")
     public String showHome() {
         return "home";
--- src/main/java/tr/com/d3kod/mvc_security/security/SecurityConfiguration.java
+++ src/main/java/tr/com/d3kod/mvc_security/security/SecurityConfiguration.java
@@ -1,40 +1,49 @@
 package tr.com.d3kod.mvc_security.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder().username("john").password("{noop}qaz").roles("PERSONEL").build();
-        UserDetails mary = User.builder().username("mary").password("{noop}qaz").roles("PERSONEL", "MUDUR").build();
-        UserDetails susan = User.builder().username("susan").password("{noop}qaz")
-                .roles("PERSONEL", "MUDUR", "MUMKUNMUDUR").build();
+        @Bean
+        public UserDetailsManager userDetailsManager(DataSource dataSource) {
+                return new JdbcUserDetailsManager(dataSource);
+        }
 
-        return new InMemoryUserDetailsManager(john, mary, susan);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                configurer -> configurer
-                        .requestMatchers("/").hasRole("PERSONEL")
-                        .requestMatchers("/leaders/**").hasRole("MUDUR")
-                        .requestMatchers("/systems/**").hasRole("MUMKUNMUDUR")
-                        .anyRequest().authenticated())
-                .formLogin(
-                        form -> form.loginPage("/login")
-                                .loginProcessingUrl("/authenticate")
-                                .permitAll())
-                .logout(logout -> logout.permitAll())
-                .exceptionHandling(configurer -> configurer.accessDeniedPage("/access-denied"));
-        return http.build();
-    }
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http.authorizeHttpRequests(
+                                configurer -> configurer
+                                                .requestMatchers("/").hasRole("PERSONEL")
+                                                .requestMatchers("/leaders/**").hasRole("MUDUR")
+                                                .requestMatchers("/systems/**").hasRole("MUMKUNMUDUR")
+                                                .anyRequest().authenticated())
+                                .formLogin(
+                                                form -> form.loginPage("/login")
+                                                                .loginProcessingUrl("/authenticate")
+                                                                .permitAll())
+                                .logout(logout -> logout.permitAll())
+                                .exceptionHandling(configurer -> configurer.accessDeniedPage("/access-denied"));
+                return http.build();
+        }
 }
+
+// @Bean
+// public InMemoryUserDetailsManager userDetailsManager() {
+// UserDetails john =
+// User.builder().username("john").password("{noop}qaz").roles("PERSONEL").build();
+// UserDetails mary =
+// User.builder().username("mary").password("{noop}qaz").roles("PERSONEL",
+// "MUDUR").build();
+// UserDetails susan = User.builder().username("susan").password("{noop}qaz")
+// .roles("PERSONEL", "MUDUR", "MUMKUNMUDUR").build();
+
+// return new InMemoryUserDetailsManager(john, mary, susan);
+// }
